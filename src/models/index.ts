afefe523b007922bb72303c5db234fa4207b456a
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-06 19:48:40 +0200

    fix(models): models index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,4 @@
 export { default as UserModel } from './User.model';
-export { default as test } from './User.model';
+export { default as LobbyModel } from './Lobby.model';
+export { default as ChannelModel } from './Channel.model';
+export { default as MessageModel } from './Message.model';
