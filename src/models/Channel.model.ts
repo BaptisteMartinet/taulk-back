87cc3c9d9c36c9c11d86d57d955601c65c69fefb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-06 19:48:14 +0200

    feat(models): change to Channel model

--- src/models/Channel.model.ts
+++ src/models/Channel.model.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import { ILobby } from './Lobby.model';
 import type { IMessage } from './Message.model';
 import type { IUser } from './User.model';
 
 export interface IChannel {
+  lobby: ILobby;
   title: String;
   owner: IUser;
   users: Array<IUser>;
@@ -10,6 +12,7 @@ export interface IChannel {
 }
 
 const ChannelSchema = new mongoose.Schema<IChannel>({
+  lobby: { type: mongoose.Types.ObjectId, ref: 'Lobby', require: true },
   title: { type: String, required: true },
   owner: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
   users: [{ type: mongoose.Types.ObjectId, ref: 'User', required: true }],
