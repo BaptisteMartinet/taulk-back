601067f8e3fd685f36a57ee11b3ca108aa85274b
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-06 19:47:50 +0200

    feat(models): change to Message model

--- src/models/Message.model.ts
+++ src/models/Message.model.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 import type { IUser } from './User.model';
+import type { IChannel } from './Channel.model';
 
 export interface IMessage {
-  content: string,
-  owner: IUser,
+  channel: IChannel;
+  content: string;
+  owner: IUser;
 }
 
 const MessageSchema = new mongoose.Schema<IMessage>({
+  channel: { type: mongoose.Types.ObjectId, ref: 'Channel', required: true },
   content: { type: String, required: true },
   owner: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
 }, { timestamps: true });
