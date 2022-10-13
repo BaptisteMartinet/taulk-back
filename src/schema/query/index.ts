26cfb5ad27d498be9e7fb6b0f7f53d7457d15f9e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-10-13 22:15:48 +0200

    feat(schema/query): implement myLobbies query

--- src/schema/query/index.ts
+++ src/schema/query/index.ts
@@ -1,14 +1,34 @@
 import { GraphQLList, GraphQLObjectType } from 'graphql';
+import type { IContext } from 'utils/context';
 import LobbyModel from 'models/Lobby.model';
-import { LobbyType } from 'schema/output-types';
+import { LobbyType, LobbyFullType } from 'schema/output-types';
 
 const QueryType = new GraphQLObjectType({
   name: 'Query',
   fields: {
     lobbies: {
       type: new GraphQLList(LobbyType),
-      async resolve(_, args, ctx) {
-        const lobbies = await LobbyModel.find().populate('owner users channels');
+      async resolve() {
+        const lobbies = await LobbyModel.find();
+        return lobbies;
+      },
+    },
+    myLobbies: {
+      type: new GraphQLList(LobbyFullType),
+      async resolve(_, args, ctx: IContext) {
+        const { currentUser } = ctx;
+        if (!currentUser) {
+          throw new Error('User must be authenticated');
+        }
+        const lobbies = await LobbyModel.find({ users: currentUser.id }).populate([
+          'owner',
+          'users',
+          {
+            path: 'channels',
+            model: 'Channel',
+            populate: 'owner users messages',
+          },
+        ]);
         return lobbies;
       },
     },
